8cadcf30f58e71a64dbeca44207376411d1bb4cd
Alexandru - Cătălin OPRIȘA <alexandru.oprisa@stud.etti.upb.ro>
2020-08-28 18:36:53 +0300

    Connection to database

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.Remoting.Proxies;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
 
 namespace Client
 {
@@ -14,11 +17,66 @@ namespace Client
         {
             InstanceContext context = new InstanceContext(new MyCallback());
             Proxy.ChatServiceClient server = new Proxy.ChatServiceClient(context);
+            var x = 0;
 
-            Console.WriteLine("Enter username");
-            var username = Console.ReadLine();
-            server.Join(username);
 
+            MySqlConnection connection = new MySqlConnection("datasource=localhost;Initial Catalog=clienti;port=3306;username=root;password=pleomax1");
+            connection.Open();
+            if( connection.State == ConnectionState.Open)
+            {
+                Console.WriteLine("Connection to Database is open");
+                Console.WriteLine("Please insert your username and password:");
+                var username = Console.ReadLine();
+                var password = Console.ReadLine();
+
+                string selectQuery= $"Select * FROM datelogare Where username = '{username}'";
+                using (var cmd = new MySqlCommand(selectQuery, connection))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            x++;
+                            var parola = reader.GetString(2);
+                            if (password == parola)
+                            {
+                                MessageBox.Show("Valid credentials. Welcome to chat:");
+                            }
+                            else if (password != parola)
+                            {
+                                MessageBox.Show("Incorect password");
+                                return ;
+                            }
+                        }
+                        if (x == 0)
+                        {
+                            reader.Close();
+                            string insertQuery = $"Insert into datelogare (username, password) Values ( '{username}', '{password}' )";
+                            using (var command = new MySqlCommand(insertQuery, connection))
+                            {
+                                if (command.ExecuteNonQuery() == 1)
+                                {
+                                    MessageBox.Show("Your Account does not exist. I will register you to the database now.");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Registration failed");
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+                server.Join(username);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Connection");
+            }
+
+            
             Console.WriteLine();
             Console.WriteLine("Enter Message");
             Console.WriteLine("Press Q to Exit");
